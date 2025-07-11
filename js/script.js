c7c345710d66a66eec23a9b04037437aa4b1520e
atulscript222 <atulprajapathi080x@gmail.com>
2025-07-12 01:57:41 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// script.js
+function showSection(id) {
+    document.querySelectorAll('section').forEach(section => {
+        section.classList.remove('active');
+    });
+    document.getElementById(id).classList.add('active');
+}
+
